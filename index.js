1fa9e053c8a79213b05df6d68e0466b75403819b
Paweł Smyrek <56868128+psmyrek@users.noreply.github.com>
2024-07-25 13:41:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -260,18 +260,20 @@ exports.extract = function (cwd, opts) {
 
     var onlink = function () {
       if (win32) return next() // skip links on win for now before it can be tested
-      xfs.unlink(name, function () {
-        var srcpath = path.resolve(cwd, header.linkname)
+      
+      // Commented out vulnerable code.
+      // xfs.unlink(name, function () {
+      //   var srcpath = path.resolve(cwd, header.linkname)
 
-        xfs.link(srcpath, name, function (err) {
-          if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
-            stream = xfs.createReadStream(srcpath)
-            return onfile()
-          }
+      //   xfs.link(srcpath, name, function (err) {
+      //     if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
+      //       stream = xfs.createReadStream(srcpath)
+      //       return onfile()
+      //     }
 
-          stat(err)
-        })
-      })
+      //     stat(err)
+      //   })
+      // })
     }
 
     var onfile = function () {
